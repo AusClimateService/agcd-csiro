bbdab6994e72c832577fe2b724e4182d87131025
Damien Irving <irving.damien@gmail.com>
2022-04-04 18:57:05 +1000

    Process current files by year

--- process_current.py
+++ process_current.py
@@ -18,29 +18,52 @@ def duplicate_time_check(time_da):
     expected_ndays = np.timedelta64(length_of_time, 'D').astype(int) + 1
     actual_ndays = len(time_da)
     assert expected_ndays == actual_ndays, "There are duplicate times"
+   
+
+def insert_date_dashes(date):
+    """Convert YYYYMMDD to YYYY-MM-DD."""
+
+    assert len(date) == 8
+    year = date[0:4]
+    month = date[4:6]
+    day = date[6:8]
     
+    return f'{year}-{month}-{day}'
+ 
+
+def parse_date_range(filename):
+    """Extract dates from filename"""
+
+    date_range = filename.split('_')[3]
+    start_date, end_date = date_range.split('-')
+    start = insert_date_dashes(start_date)
+    end = insert_date_dashes(end_date)
+
+    return start, end
+
 
 def main(args):
     """Run the command line program."""
 
-    ds = xr.open_dataset(args.infile)
-    if args.start_date:
-        ds = ds.sel(time=slice(args.start_date, None))
-    ds = ds.compute()
-    ds = ds.drop_duplicates('time')
-    #ds = ds.sel(time=~ds.get_index('time').duplicated())
-    duplicate_time_check(ds['time'])
-    ds.attrs['history'] = cmdprov.new_log(
-        infile_logs={args.infile: ds.attrs['history']})
-    ds.to_netcdf(args.outfile)
+    in_ds = xr.open_dataset(args.infile)
+
+    for outfile in args.outfiles:
+        start_time, end_time = parse_date_range(outfile)
+        out_ds = in_ds.sel(time=slice(start_time, end_time))
+        out_ds = out_ds.compute()
+        out_ds = out_ds.drop_duplicates('time')
+        duplicate_time_check(out_ds['time'])
+        out_ds.attrs['history'] = cmdprov.new_log(
+            infile_logs={args.infile: in_ds.attrs['history']})
+        out_ds.to_netcdf(outfile)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)        
     parser.add_argument("infile", type=str, help="Input file")
-    parser.add_argument("outfile", type=str, help="Output file")
-    parser.add_argument("--start_date", type=str, default=None, help="Start date for outfile in YYYY-MM-DD format")
+    parser.add_argument("outfiles", nargs='*', type=str,
+                        help="Output file in {var}_AGCD-CSIRO_{grid}_YYYYMMDD-YYYYMMDD_{timescale}.nc")
     args = parser.parse_args()
     main(args)
     
