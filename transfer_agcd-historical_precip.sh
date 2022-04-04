ee3dc3be7eba13f5e71b6493bf58e7197be0632b
Damien Irving <irving.damien@gmail.com>
2022-04-04 14:53:52 +1000

    Update transfer_agcd-historical_precip.sh

--- transfer_agcd-historical_precip.sh
+++ transfer_agcd-historical_precip.sh
@@ -1,13 +1,31 @@
 # Transfer historical AGCD precipitation data from Petricor to Gadi
 #
-# Usage: $ bash transfer_agcd-historical_precip.sh {nci_username} {nci_password}
+# Usage: $ bash transfer_agcd-historical_precip.sh {variable} {nci_username} {nci_password}
 
-nci_username=$1
-nci_password=$2
+variable=$1
+nci_username=$2
+nci_password=$3
 
-for year in $(seq 1900 2018); do 
+if [ "${variable}" == "precip" ]; then
+        vardir="daily-rainfall"
+	start_year=1900
+elif [ "${variable}" == "tmax" ]; then
+        vardir="daily-temperature/maximum-temperature"
+	start_year=1910
+elif [ "${variable}" == "tmin" ]; then
+        vardir="daily-temperature/minimum-temperature"
+	start_year=1910
+elif [ "${variable}" == "vp-9am" ]; then
+        vardir="daily-vapour-pressure/vp-9am"
+	start_year=1971
+elif [ "${variable}" == "vp-3pm" ]; then
+        vardir="daily-vapour-pressure/vp-3pm"
+	start_year=1971
+fi
+
+for year in $(seq ${start_year} 2018); do 
 	echo ${year}
-        csirofile=/datasets/work/af-cdp/work/agcd/HISTORICAL/daily-rainfall/merged/${year}.nc
-	ncifile=/g/data/xv83/agcd-csiro/precip/precip-total_AGCD-CSIRO_r005_${year}0101-${year}1231_daily.nc
+        csirofile=/datasets/work/af-cdp/work/agcd/HISTORICAL/${vardir}/merged/${year}.nc
+	ncifile=/g/data/xv83/agcd-csiro/${variable}/${variable}_AGCD-CSIRO_r005_${year}0101-${year}1231_daily.nc
 	sshpass -p ${nci_password} scp ${csirofile} ${nci_username}@gadi.nci.org.au:${ncifile}
 done
